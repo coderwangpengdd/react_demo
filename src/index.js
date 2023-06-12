53321bde30ed22f43228aa862d77f21b78fe9683
王鹏 <peng.wang@bigmarker.com>
2023-06-12 18:13:32 +0800

    update file

---
+++ src/index.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: 王鹏 peng.wang@bigmarker.com
+ * @Date: 2023-06-12 16:35:35
+ * @LastEditors: 王鹏 peng.wang@bigmarker.com
+ * @LastEditTime: 2023-06-12 17:23:51
+ * @FilePath: /react_demo/src/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>React学习</div>
+  </React.StrictMode>
+);
